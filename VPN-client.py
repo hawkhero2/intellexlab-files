d18f25923d8d1be224b66fc0dc9a551deef1a39a
hawkhero2 <cheseecheseechesee@gmail.com>
2023-05-17 23:18:08 +0300

    small update

--- VPN-client.py
+++ VPN-client.py
@@ -13,16 +13,17 @@ def button_function():
 # acc_label = customtkinter.CTkLabel(master=app, text="Account")
 
 acc_input = customtkinter.CTkEntry(app, placeholder_text="account")
-acc_input.place(in_ = app, relx = 0.3, rely = 0.3, anchor = tkinter.N)
+acc_input.place(in_ = app, relx = 0.1, rely = 0.1)
+
 pwd_input = customtkinter.CTkEntry(app, placeholder_text = "password")
-pwd_input.place(in_=app, relx=0.2, rely=0.2, anchor = tkinter.N)
+pwd_input.place(in_=app, relx=0.1, rely=0.17)
 
 # textbox.place(in_=app, relx=0.2, rely=0.1,anchor=tkinter.N)
 # acc_label.place(in_=app, relx=0.4,rely=0.4,anchor=tkinter.CENTER)
 
 # Use CTkButton instead of tkinter Button
-button = customtkinter.CTkButton(master = app, text = "CTkButton", command = button_function())
-button.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
+button = customtkinter.CTkButton(master = app, text = "Connect", command = button_function)
+button.place(relx=0.1, rely=0.25)
 
 # app._deactivate_windows_window_header_manipulation = True
 
