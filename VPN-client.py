19d6102811bf2d415bbb408f6e185cb09bab0cee
Ivanciu Andrei <47902631+hawkhero2@users.noreply.github.com>
2023-05-20 21:29:49 +0300

    removed class

--- VPN-client.py
+++ VPN-client.py
@@ -5,49 +5,41 @@ import customtkinter
 customtkinter.set_appearance_mode("System")  # Modes: system (default), light, dark
 customtkinter.set_default_color_theme("blue")  # Themes: blue (default), dark-blue, green
 
-class VpnStatus(customtkinter.CTk):
-
-    def __init__(self):
-        super().__init__()
-        self.geometry("500x250")
-        self.title("VPN")
-
-        self.disconnect_btn = customtkinter.CTkButton(self,text = "Disconnect",command = button_disconnect_vpn)
-        self.disconnect_btn.place( relx = 0.5, rely = 0.5)
-        self.mainloop()
-
 def button_disconnect_vpn():
     print("you disconnected the vpn")
-    app.iconify()
+    login.iconify()
+    vpn_status.withdraw()
     
-    
-    # check Softether page to make use of the terminal commands?
-    # https://www.softether.org/4-docs/1-manual/7._Installing_SoftEther_VPN_Server/7.3_Install_on_Linux_and_Initial_Configurations
+vpn_status = customtkinter.CTk()
+vpn_status.geometry("500x250")
+vpn_status.title("VPN")
+vpn_status.resizable(False,False)
 
-app = customtkinter.CTk()  # create CTk window like you do with the Tk window
-app.geometry("300x300")
-app.title("VPN")
+disconnect_btn = customtkinter.CTkButton(vpn_status,text = "Disconnect",command = button_disconnect_vpn)
+disconnect_btn.place( relx = 0.5, rely = 0.5)
 
-def button_function():
-    print("Hello there: "+acc_input.get())
-    app.withdraw()
-    vpn_status = VpnStatus()
+login = customtkinter.CTk()  # create CTk window like you do with the Tk window
+login.geometry("300x300")
+login.title("VPN")
 
-# acc_label = customtkinter.CTkLabel(master=app, text="Account")
+def connect_button():
+    print("You connected " + acc_input.get())
+    login.withdraw()
+    vpn_status.iconify()
 
-acc_input = customtkinter.CTkEntry(app, placeholder_text = "account")
-acc_input.place(in_ = app, relx = 0.25, rely = 0.3)
+acc_input = customtkinter.CTkEntry(login, placeholder_text = "account")
+acc_input.place(in_ = login, relx = 0.25, rely = 0.3)
 
-pwd_input = customtkinter.CTkEntry(app, placeholder_text = "password")
-pwd_input.place(in_=app, relx=0.25, rely=0.4)
+pwd_input = customtkinter.CTkEntry(login, placeholder_text = "password")
+pwd_input.place(in_=login, relx=0.25, rely=0.4)
 
 
 
 # Use CTkButton instead of tkinter Button
-button = customtkinter.CTkButton(master = app, text = "Connect", command = button_function)
+button = customtkinter.CTkButton(master = login, text = "Connect", command =connect_button)
 button.place(relx=0.25, rely=0.6)
 
-# app._deactivate_windows_window_header_manipulation = True
+# login._deactivate_windows_window_header_manipulation = True
 
-app.resizable(False,False)
-app.mainloop()
+login.resizable(False,False)
+login.mainloop()
