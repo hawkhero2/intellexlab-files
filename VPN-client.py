e0295fd3fddc2e627d6b6c6e9a35b614c017e464
hawkhero2 <cheseecheseechesee@gmail.com>
2023-05-22 18:42:00 +0300

    .

--- VPN-client.py
+++ VPN-client.py
@@ -1,4 +1,3 @@
-
 import tkinter
 from typing import Optional, Tuple, Union
 import customtkinter
@@ -6,13 +5,19 @@ import customtkinter
 customtkinter.set_appearance_mode("System")  # Modes: system (default), light, dark
 customtkinter.set_default_color_theme("blue")  # Themes: blue (default), dark-blue, green
 
+def get_conn_status():
+    status : bool = True
+    return status
+
+connection_status : bool = get_conn_status()
+
 credentials = {
     "acc":"",
     "pw":""
 }
 
 def check_creds():
-    login.iconify()
+    creds.iconify()
     vpn_status.withdraw()
 
 def button_disconnect_vpn():
@@ -23,39 +28,40 @@ vpn_status.geometry("500x250")
 vpn_status.title("VPN")
 vpn_status.resizable(False,False)
 
-disconnect_btn = customtkinter.CTkButton(vpn_status,text = "Disconnect",command = button_disconnect_vpn)
-disconnect_btn.place(relx = 0.7, rely = 0.8)
-creds_btn = customtkinter.CTkButton( vpn_status, text = "Settings", command = check_creds )
-creds_btn.place(relx = 0.4, rely = 0.8)
+if(connection_status == True):
+    pass
 
+
+disconnect_btn = customtkinter.CTkButton(vpn_status,text = "Disconnect",command = button_disconnect_vpn)
+disconnect_btn.place(relx = 0.65, rely = 0.8)
+creds_btn = customtkinter.CTkButton( vpn_status, text = "Settings", command = check_creds )
+creds_btn.place(relx = 0.35, rely = 0.8)
+disconnect_btn._text_color_disabled
 
 # ------------------------------------------------------------------
-login = customtkinter.CTk() 
-login.geometry("300x300")
-login.title("VPN")
-login.withdraw()
+creds = customtkinter.CTk() 
+creds.geometry("300x300")
+creds.title("VPN")
+creds.withdraw()
 
 def save_creds():
     # print("You connected " + acc_input.get())
     credentials.__setitem__("acc", acc_input.get())
     credentials.__setitem__("pw", pwd_input.get())
 
-    login.withdraw()
+    print("your login credentials are: acc:"+credentials.get("acc")+" with the password: "+credentials.get("pw"))
+    creds.withdraw()
     vpn_status.iconify()
 
-acc_input = customtkinter.CTkEntry(login, placeholder_text = "account")
-acc_input.place(in_ = login, relx = 0.25, rely = 0.3)
+acc_input = customtkinter.CTkEntry(creds, placeholder_text = "account")
+acc_input.place(in_ = creds, relx = 0.25, rely = 0.3)
 
-pwd_input = customtkinter.CTkEntry(login, placeholder_text = "password")
-pwd_input.place(in_=login, relx=0.25, rely=0.4)
+pwd_input = customtkinter.CTkEntry(creds, placeholder_text = "password")
+pwd_input.place(in_=creds, relx=0.25, rely=0.4)
 
 
-
-# Use CTkButton instead of tkinter Button
-button = customtkinter.CTkButton(master = login, text = "Connect", command =save_creds)
+button = customtkinter.CTkButton(master = creds, text = "Connect", command = save_creds)
 button.place(relx=0.25, rely=0.6)
 
-# login._deactivate_windows_window_header_manipulation = True
-
-login.resizable(False,False)
+creds.resizable(False,False)
 vpn_status.mainloop()
