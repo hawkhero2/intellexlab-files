c708ccb61d3c5040b6739a9815662c5998a0bcbd
hawkhero2 <cheseecheseechesee@gmail.com>
2023-05-22 23:07:17 +0300

    cleaning

--- VPN-client.py
+++ VPN-client.py
@@ -2,8 +2,8 @@ import tkinter
 from typing import Optional, Tuple, Union
 import customtkinter
 
-customtkinter.set_appearance_mode("System")  # Modes: system (default), light, dark
-customtkinter.set_default_color_theme("blue")  # Themes: blue (default), dark-blue, green
+customtkinter.set_appearance_mode("System")
+customtkinter.set_default_color_theme("blue")
 
 def get_conn_status():
     status : bool = True
@@ -32,9 +32,15 @@ if(connection_status == True):
     pass
 
 
-disconnect_btn = customtkinter.CTkButton(vpn_status,text = "Disconnect",command = button_disconnect_vpn)
+disconnect_btn = customtkinter.CTkButton(vpn_status, 
+                                         text = "Disconnect", 
+                                         command = button_disconnect_vpn)
+
 disconnect_btn.place(relx = 0.65, rely = 0.8)
-creds_btn = customtkinter.CTkButton( vpn_status, text = "Settings", command = check_creds )
+creds_btn = customtkinter.CTkButton(vpn_status, 
+                                    text = "Settings", 
+                                    command = check_creds)
+
 creds_btn.place(relx = 0.35, rely = 0.8)
 disconnect_btn._text_color_disabled
 
@@ -49,7 +55,10 @@ def save_creds():
     credentials.__setitem__("acc", acc_input.get())
     credentials.__setitem__("pw", pwd_input.get())
 
-    print("your login credentials are: acc:"+credentials.get("acc")+" with the password: "+credentials.get("pw"))
+    print("your login credentials are: acc:"+
+          credentials.get("acc")+
+          " with the password: "+credentials.get("pw"))
+    
     creds.withdraw()
     vpn_status.iconify()
 
@@ -60,7 +69,10 @@ pwd_input = customtkinter.CTkEntry(creds, placeholder_text = "password")
 pwd_input.place(in_=creds, relx=0.25, rely=0.4)
 
 
-button = customtkinter.CTkButton(master = creds, text = "Connect", command = save_creds)
+button = customtkinter.CTkButton(master = creds, 
+                                 text = "Connect", 
+                                 command = save_creds)
+
 button.place(relx=0.25, rely=0.6)
 
 creds.resizable(False,False)
